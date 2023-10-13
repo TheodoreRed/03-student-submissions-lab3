0c8a0553607d9e84f4f49bcab3d42aac02958bae
Theodore Redlon <redlontheo@gmail.com>
2023-10-13 09:17:39 -0400

    Optimize solution

--- script.js
+++ script.js
@@ -7,12 +7,11 @@ const student4 = { name: "Jill", score: 88, passed: true };
 const submissions = [student1, student2, student3, student4];
 
 const addSubmission = (array, newName, newScore) => {
-  const newSubmission = {
+  array.push({
     name: newName,
     score: newScore,
     passed: newScore >= 60,
-  };
-  array.push(newSubmission);
+  });
 };
 console.log(submissions);
 addSubmission(submissions, "Bill", 60);
